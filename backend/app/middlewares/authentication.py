43bc012755815a8535905b71e2bf44616d1ed571
eedu7 <mueedahmad067@gmail.com>
2025-02-19 18:30:01 +0500

    FIX: fixed the middleware
    it was not returning a tuple, but only a boolean value

--- backend/app/middlewares/authentication.py
+++ backend/app/middlewares/authentication.py
@@ -1,24 +1,42 @@
+from typing import Tuple
+from uuid import UUID
+
+from jose import JWTError
 from starlette.authentication import AuthenticationBackend
 from starlette.middleware.authentication import \
     AuthenticationMiddleware as BaseAuthenticationMiddleware
 from starlette.requests import HTTPConnection
 
+from app.schemas.user import CurrentUser
+from app.utils import JWTHandler
+
 
 class AuthBackend(AuthenticationBackend):
-    async def authenticate(self, conn: HTTPConnection) -> bool:
+    async def authenticate(
+        self, conn: HTTPConnection
+    ) -> Tuple[bool, CurrentUser | None]:
+        current_user = CurrentUser()
         authorization = conn.headers.get("Authorization")
         if not authorization:
-            return False
+            return False, current_user
 
         try:
             scheme, token = authorization.split()
             if scheme.lower() != "bearer":
-                return False
+                return False, current_user
         except ValueError:
-            return False
+            return False, current_user
 
         if not token:
-            return False
+            return False, current_user
+
+        try:
+            payload = JWTHandler.decode(token)
+            user_uuid = payload.get("uuid")
+        except JWTError:
+            return False, current_user
+        current_user.uuid = UUID(user_uuid)
+        return True, current_user
 
 
 class AuthenticationMiddleware(BaseAuthenticationMiddleware):
