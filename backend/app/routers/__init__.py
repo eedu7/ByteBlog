ece7f7aec8f9e6b446694c5835c042d3c4690074
eedu7 <mueedahmad067@gmail.com>
2025-02-19 20:40:18 +0500

    FEAT: Added the test api for the fasatpi app

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,4 +1,7 @@
+from http import HTTPStatus
+
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 
 from .auth import auth_router
 from .users import user_router
@@ -7,3 +10,8 @@ router = APIRouter()
 
 router.include_router(auth_router, prefix="/auth", tags=["Authentication"])
 router.include_router(user_router, prefix="/user", tags=["User"])
+
+
+@router.get("/")
+async def index():
+    return JSONResponse(status_code=HTTPStatus.OK, content={"message": "OK"})
