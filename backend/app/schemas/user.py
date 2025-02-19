c1ecda6666f79b8b708064bbc2ed798e837ce39a
eedu7 <mueedahmad067@gmail.com>
2025-02-19 15:00:21 +0500

    MINOR: refactored the code base, and created the AuthResponse

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -3,17 +3,6 @@ from uuid import UUID
 from pydantic import BaseModel, EmailStr, Field
 
 
-class LoginUserRequest(BaseModel):
-    email: EmailStr = Field(
-        ..., description="User email address", examples=["john.doe@example.com"]
-    )
-    password: str = Field(..., description="User password", examples=["Password@123"])
-
-
-class RegisterUserRequest(LoginUserRequest):
-    username: str = Field(..., description="User username", examples=["john.doe"])
-
-
 class UserResponse(BaseModel):
     email: EmailStr = Field(
         ..., description="User email address", examples=["john.doe@example.com"]
