88eb805f60dd5f047a9d7cdcff6637fc62aba1f4
eedu7 <mueedahmad067@gmail.com>
2025-02-19 09:57:23 +0500

    MODIFY: modified the register_user method, to check if the user exists or not, also encrypt password, before saving

--- backend/app/crud/user.py
+++ backend/app/crud/user.py
@@ -1,11 +1,9 @@
-from typing import Any, Dict
-
 from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.base import BaseCRUD
 from app.models import User
-
+from app.utils import PasswordHandler
 
 class UserCRUD(BaseCRUD[User]):
     def __init__(self, session: AsyncSession):
@@ -17,7 +15,15 @@ class UserCRUD(BaseCRUD[User]):
         except Exception as e:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
-    async def register_user(self, data: Dict[str, Any]):
-        user = await super().create(attributes=data)
-        print(user)
+    async def register_user(self, email: str, password: str, username: str) -> User:
+        user = await self.get_by_email(email)
+        if user:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User already registered")
+        hashed_password = PasswordHandler.hash(password)
+        
+        user = await super().create({
+            "username": username,
+            "email": email,
+            "password": hashed_password
+        })
         return user
