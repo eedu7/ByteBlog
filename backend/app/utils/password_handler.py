fa089cb8dacff0a2ec977c3ddca1fba3af4a22d0
eedu7 <mueedahmad067@gmail.com>
2025-02-19 13:04:15 +0500

    MINOR: Code format and sorting

--- backend/app/utils/password_handler.py
+++ backend/app/utils/password_handler.py
@@ -3,13 +3,11 @@ from passlib.context import CryptContext
 
 class PasswordHandler:
     pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-    
+
     @staticmethod
     def hash(password: str) -> str:
         return PasswordHandler.pwd_context.hash(password)
-    
+
     @staticmethod
     def verify(password: str, hashed_password: str) -> bool:
         return PasswordHandler.pwd_context.verify(password, hashed_password)
-    
-    
