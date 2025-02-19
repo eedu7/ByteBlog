6b374049bb093370fbdcd7be026bbe09011f5753
eedu7 <mueedahmad067@gmail.com>
2025-02-19 20:30:25 +0500

    MINOR: added pytest and wrote test for password handler

---
+++ backend/tests/utils/test_password_handler.py
@@ -0,0 +1,20 @@
+from app.utils import PasswordHandler
+
+
+def test_password_hashing():
+    password = "password"
+    hashed_password = PasswordHandler.hash(password)
+    assert isinstance(hashed_password, str)
+    assert hashed_password != ""
+    assert hashed_password != password
+
+
+def test_password_verification():
+    password = "password"
+    hashed_password = PasswordHandler.hash(password)
+
+    assert PasswordHandler.verify(password, hashed_password)
+
+    incorrect_password = "wrong_password"
+
+    assert not PasswordHandler.verify(incorrect_password, hashed_password)
