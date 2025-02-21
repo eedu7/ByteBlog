f466da200de33e431c0624bd7a6def7f4716ba6e
eedu7 <mueedahmad067@gmail.com>
2025-02-21 16:14:27 +0500

    MINOR: Added docstring to the utils JWTHandler and PasswordHandler

--- backend/app/utils/jwt_handler.py
+++ backend/app/utils/jwt_handler.py
@@ -10,21 +10,49 @@ from app.exceptions import CustomException
 
 
 class JWTDecodeError(CustomException):
+    """
+    Exception raised when a JWT token cannot be decoded due to invalid format or signature.
+    """
+
     code = HTTPStatus.UNAUTHORIZED
     message = "Invalid token"
 
 
 class JWTExpiredError(CustomException):
+    """
+    Exception raised when a JWT token has expired.
+    """
+
     code = HTTPStatus.UNAUTHORIZED
     message = "Expired token"
 
 
 class JWTHandler:
+    """
+    A utility class for handling JSON Web Token (JWT) encoding and decoding.
+
+    This class provides methods to generate and validate JWTs using a secret kwt and a specified algorithm. It includes built-in expiration handling to ensure security.
+
+    Attributes:
+        secret_key (str): The secret kwy used for signing JWTs.
+        algorithm (str): The algorithm used for encoding and decoding JWTs.
+    """
+
     secret_key = config.JWT_SECRET_KEY
     algorithm = config.JWT_ALGORITHM
 
     @staticmethod
-    def encode(payload: dict, expire_in_min: int = 60) -> str:
+    def encode(payload: Dict[str, Any], expire_in_min: int = 60) -> str:
+        """
+        Generates a JWT with the given payload and expiration time.
+
+        Args:
+            payload (Dict[str, Any]): The data to include in the JWT.
+            expire_in_min (int, optional): The expiration time in minutes. Defaults to `60`.
+
+        Returns:
+            str: The encoded JWT as string
+        """
         time_of_encoding = datetime.now(UTC)
         expire = time_of_encoding + timedelta(expire_in_min)
 
@@ -36,6 +64,19 @@ class JWTHandler:
 
     @staticmethod
     def decode(token: str) -> Dict[str, Any]:
+        """
+        Decodes and verifies a JWT token.
+
+        Args:
+            token (str): The JWT token to decode.
+
+        Returns:
+            Dict[str, Any]: The decoded payload if the token is valid.
+
+        Raises:
+            JWTExpiredError: If the token has expired.
+            JWTDecodeError: If the token is invalid or cannot be decoded.
+        """
         try:
             return jwt.decode(
                 token, JWTHandler.secret_key, algorithms=[JWTHandler.algorithm]
--- backend/app/utils/password_handler.py
+++ backend/app/utils/password_handler.py
@@ -2,12 +2,42 @@ from passlib.context import CryptContext
 
 
 class PasswordHandler:
+    """
+    A utility class for handling password hashing and verification.
+
+    This class uses the `passlib` library with the `bcrypt` hashing algorithm
+    to securely hash password and verify them.
+
+    Attributes:
+        pwd_context (CryptContext): A configured instance of CryptContext that
+        handles password hashing and verification.
+    """
+
     pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
     @staticmethod
     def hash_password(password: str) -> str:
+        """
+        Hash a plain-text password using the bcrypt algorithm.
+
+        Args:
+            password (str): The plain-text password to be hashed.
+
+        Returns:
+            str: The hashed password, which can be stored securely.
+        """
         return PasswordHandler.pwd_context.hash(password)
 
     @staticmethod
     def verify_password(password: str, hashed_password: str) -> bool:
+        """
+        Verify whether a given plain-text password matches a hashed password.
+
+        Args:
+            password (str): The plain-text password to verify.
+            hashed_password (str): The previously hashed password to compare.
+
+        Returns:
+            bool: `True` if the password matches the hash, `False` otherwise.
+        """
         return PasswordHandler.pwd_context.verify(password, hashed_password)
