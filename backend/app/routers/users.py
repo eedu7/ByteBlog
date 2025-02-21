daf13f0e9d4f41c922458a214fdb8aa9aa9e16e7
eedu7 <mueedahmad067@gmail.com>
2025-02-21 17:13:47 +0500

    MINOR: Added docs

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -8,6 +8,6 @@ from app.schemas.user import UserResponse
 user_router = APIRouter(dependencies=[Depends(AuthenticationRequired)])
 
 
-@user_router.get("/user-profile/", response_model=UserResponse)
+@user_router.get("/user-profile", response_model=UserResponse)
 async def get_current_user(current_user: User = Depends(get_current_user)):
     return current_user
