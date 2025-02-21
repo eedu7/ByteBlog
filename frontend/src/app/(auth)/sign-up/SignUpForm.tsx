03069ad65edae0bcc719cd73a8104c58d6094e72
eedu7 <mueedahmad067@gmail.com>
2025-02-21 20:24:11 +0500

    FEAT: Added the SignUpForm

---
+++ frontend/src/app/(auth)/sign-up/SignUpForm.tsx
@@ -0,0 +1,92 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+    username: z.string().min(2).max(50),
+    email: z.string().email(),
+    password: z.string().min(8).max(16),
+});
+
+const SignUpForm = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+            email: "",
+            password: "",
+        },
+        mode: "onChange",
+    });
+
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+        console.table(values);
+    };
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="soace-y-8">
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder="john.doe" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email Address</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="john.doe@example.com"
+                                    {...field}
+                                    type="email"
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Some Password"
+                                    {...field}
+                                    type="password"
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Register</Button>
+            </form>
+        </Form>
+    );
+};
+
+export default SignUpForm;
