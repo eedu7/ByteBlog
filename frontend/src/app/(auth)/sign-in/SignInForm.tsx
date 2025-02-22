f2843ee4bedf19ee27ae253bb4e7802a2df007a2
eedu7 <mueedahmad067@gmail.com>
2025-02-22 14:21:13 +0500

    MINOR: Code refactored

--- frontend/src/app/(auth)/sign-in/SignInForm.tsx
+++ frontend/src/app/(auth)/sign-in/SignInForm.tsx
@@ -3,12 +3,69 @@
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
+import type { loginFormSchema } from "@/features/auth/formSchema";
+import useFormSchema from "@/features/auth/useFormSchema";
 
 const SignInForm = () => {
-    return <div>SignInForm</div>;
+    const { loginForm } = useFormSchema();
+
+    const onSubmit = (values: z.infer<typeof loginFormSchema>) => {
+        console.table(values);
+    };
+
+    return (
+        <Form {...loginForm}>
+            <form
+                onSubmit={loginForm.handleSubmit(onSubmit)}
+                className="space-y-8">
+                <FormField
+                    control={loginForm.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email Address</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="john.doe@example.com"
+                                    type="email"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={loginForm.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Enter Password"
+                                    type="password"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Log In</Button>
+            </form>
+        </Form>
+    );
 };
 
 export default SignInForm;
