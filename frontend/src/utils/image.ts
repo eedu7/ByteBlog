dcca8a60a37064b44144cad3d017e1105b8ee046
eedu7 <mueedahmad067@gmail.com>
2025-02-22 12:58:03 +0500

    MINOR: added the image utils for the images

---
+++ frontend/src/utils/image.ts
@@ -0,0 +1,14 @@
+import AboutUsSVG from "../../public/assets/images/about_us.svg";
+import BlogPostSVG from "../../public/assets/images/blog_post.svg";
+import ConnectedWorldSVG from "../../public/assets/images/connected_world.svg";
+import PageNotFoundSVG from "../../public/assets/images/page_not_found.svg";
+import SignInSVG from "../../public/assets/images/sign_in.svg";
+import SocialUserSVG from "../../public/assets/images/social_user.svg";
+import WelcomeSVG from "../../public/assets/images/welcome.svg";
+
+
+const images = {
+    AboutUsSVG, BlogPostSVG, ConnectedWorldSVG, PageNotFoundSVG, SignInSVG, SocialUserSVG, WelcomeSVG
+}
+
+export default images;
