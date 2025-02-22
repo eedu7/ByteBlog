f96635ba935774adfb83fe280e23cd759c127452
eedu7 <mueedahmad067@gmail.com>
2025-02-22 12:12:39 +0500

    MINOR: Added the SignUpCard
    With the SignUpForm, Card title and description, and footer for the
    social authentication

--- frontend/src/app/(auth)/sign-up/SignUpCard.tsx
+++ frontend/src/app/(auth)/sign-up/SignUpCard.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import SignUpForm from "./SignUpForm";
 
 const SignUpCard = () => {
-  return (
-    <div>SignUpCard</div>
-  )
-}
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Join Our Community</CardTitle>
+                <CardDescription>
+                    Sign up today to get access to the latest blog posts,
+                    exclusive content, and join discussion with like-minded
+                    readers&#33;
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <SignUpForm />
+            </CardContent>
+            <CardFooter>
+                <h1 className="text-center">Social Authentication</h1>
+            </CardFooter>
+        </Card>
+    );
+};
 
-export default SignUpCard
+export default SignUpCard;
