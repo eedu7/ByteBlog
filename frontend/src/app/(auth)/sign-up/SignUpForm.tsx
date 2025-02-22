299735b0e5d57dc05bdbf43b3b4588c3dfed0367
eedu7 <mueedahmad067@gmail.com>
2025-02-22 11:56:24 +0500

    FEAT: Added the UI for the SignUpForm and also connected it with the registerForm schema

--- frontend/src/app/(auth)/sign-up/SignUpForm.tsx
+++ frontend/src/app/(auth)/sign-up/SignUpForm.tsx
@@ -1,5 +1,80 @@
+import { z } from "zod";
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+import { type registerFormSchema } from "@/features/auth/formSchema";
+import useFormSchema from "@/features/auth/useFormSchema";
+
 const SignUpForm = () => {
-    return <div>SignUpForm</div>;
+    const { registerForm } = useFormSchema();
+
+    const onSubmit = (values: z.infer<typeof registerFormSchema>) => {
+        console.table(values);
+    };
+
+    return (
+        <Form {...registerForm}>
+            <form
+                onSubmit={registerForm.handleSubmit(onSubmit)}
+                className="space-y-8">
+                <FormField
+                    control={registerForm.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder="John Doe" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={registerForm.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email Address</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="john.doe@example.com"
+                                    type="email"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={registerForm.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Enter Password"
+                                    type="password"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+            </form>
+        </Form>
+    );
 };
 
 export default SignUpForm;
