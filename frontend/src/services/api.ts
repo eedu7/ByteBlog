a12dad93bf1f298fbed671028f60f8bf4bdf3037
eedu7 <mueedahmad067@gmail.com>
2025-02-28 09:48:12 +0500

    FEAT: Added the axios interceptors to add the authorization bearer token access token if there is access token in the api request

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+import { useToken } from "@/features/auth/useToken";
+
 export const axiosClient = axios.create({
     baseURL: "http://localhost:8000",
     timeout: 5000,
@@ -7,3 +9,22 @@ export const axiosClient = axios.create({
         "Content-Type": "application/json",
     },
 });
+
+
+axiosClient.interceptors.request.use(
+    (config) => {
+
+        const { getAccessToken } = useToken();
+
+        const access_token = getAccessToken();
+
+        if (access_token) {
+            config.headers["Authorization"] = `Bearer ${access_token}`
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+
+)
