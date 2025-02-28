e41e353437f3705a5c34b61c357fc89dd56c2c1d
eedu7 <mueedahmad067@gmail.com>
2025-02-28 13:22:55 +0500

    minor: code refactored

--- frontend/src/features/auth/useUser.ts
+++ frontend/src/features/auth/useUser.ts
@@ -6,18 +6,19 @@ import { getCookie, setCookie } from "cookies-next/client";
 import { User } from "./api/auth.types";
 
 interface UserState {
-    user: User,
-    setUser: (newUser: User) => void;
-    getUser: () => User;
+  user: User;
+  setUser: (newUser: User) => void;
+  getUser: () => User;
 }
 
 export const useUser = create<UserState>((set, get) => ({
-    user: getCookie("user") ? (JSON.parse(getCookie("user") as string) as User) : { uuid: "", email: "", username: "" },
-    setUser: (newUser: User) => {
-        set({ user: newUser });
+  user: getCookie("user")
+    ? (JSON.parse(getCookie("user") as string) as User)
+    : { uuid: "", email: "", username: "" },
+  setUser: (newUser: User) => {
+    set({ user: newUser });
 
-        setCookie("user", JSON.stringify(newUser), { maxAge: 60 * 60 * 24 * 7 });
-    },
-    getUser: () => get().user
+    setCookie("user", JSON.stringify(newUser), { maxAge: 60 * 60 * 24 * 7 });
+  },
+  getUser: () => get().user,
 }));
-
