cbb9892baac9bdfa11b7e7ac5a79e650e845b531
eedu7 <mueedahmad067@gmail.com>
2025-02-28 09:32:15 +0500

    FEAT: Added the useToken for the access_token and refresh_token

--- frontend/src/features/auth/useToken.ts
+++ frontend/src/features/auth/useToken.ts
@@ -1,20 +1,38 @@
+"use client";
+
 import { create } from "zustand";
-import { Token } from "./api/auth.types";
+import { getCookie, setCookie } from "cookies-next/client";
 
 interface TokenState {
-    token: Token;
-    setToken: (newToken: Token) => void;
-    getToken: () => Token
-
+    access_token: string;
+    refresh_token: string;
+    setAccessToken: (token: string, expires_in: number) => void;
+    setRefreshToken: (token: string, expires_in: number) => void;
+    getAccessToken: () => string | null;
+    getRefreshToken: () => string | null;
 }
 
 export const useToken = create<TokenState>((set, get) => ({
-    token: {
-        access_token: "",
-        refresh_token: "",
-        expires_in: 0,
-        token_type: "bearer"
+    access_token: "",
+    refresh_token: "",
+
+    setAccessToken: (token: string, expires_in: number) => {
+        set({ access_token: token });
+        setCookie("access_token", token, { maxAge: expires_in, secure: true, sameSite: "strict" });
     },
-    setToken: () => (newToken: Token) => set({ token: newToken }),
-    getToken: () => get().token,
-}))
+
+    setRefreshToken: (token: string, expires_in: number) => {
+        set({ refresh_token: token });
+        setCookie("refresh_token", token, { maxAge: expires_in, secure: true, sameSite: "strict" });
+    },
+
+    getAccessToken: () => {
+        const access_token = get().access_token;
+        return access_token || (getCookie("access_token") as string | null);
+    },
+
+    getRefreshToken: () => {
+        const refresh_token = get().refresh_token;
+        return refresh_token || (getCookie("refresh_token") as string | null);
+    },
+}));
