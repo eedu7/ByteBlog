6c37a99d1a20faae22aac6016c9dac59c38719be
eedu7 <mueedahmad067@gmail.com>
2025-02-28 13:23:08 +0500

    minor: code refactored

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,28 +3,25 @@ import axios from "axios";
 import { useToken } from "@/features/auth/useToken";
 
 export const axiosClient = axios.create({
-    baseURL: "http://localhost:8000",
-    timeout: 5000,
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: "http://localhost:8000",
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-
 axiosClient.interceptors.request.use(
-    (config) => {
+  (config) => {
+    const { getAccessToken } = useToken.getState();
 
-        const { getAccessToken } = useToken.getState();
+    const access_token = getAccessToken();
 
-        const access_token = getAccessToken();
-
-        if (access_token) {
-            config.headers["Authorization"] = `Bearer ${access_token}`
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+    if (access_token) {
+      config.headers["Authorization"] = `Bearer ${access_token}`;
     }
-
-)
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
