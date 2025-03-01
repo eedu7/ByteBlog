87be38a77e2d07137cc9a750e716388593a44ad0
eedu7 <mueedahmad067@gmail.com>
2025-03-01 10:17:08 +0500

    FEAT: added responsiveness

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -5,21 +5,17 @@ import React from "react";
 const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
     return (
         <>
-            <nav className="block text-center text-4xl font-bold font-mono h-[5vh]">
+            <nav className="block text-center font-bold font-mono h-[5vh] border border-rose-400">
                 Authentication Page
             </nav>
-            <main className="w-full h-[95vh] grid grid-cols-2 gap-2">
+            <main className="w-full h-[95vh] md:grid grid-cols-2 gap-2">
                 <section className="col-span-1 h-full">
-                    <div className="w-full h-full flex justify-center items-center">
+                    <div className="w-full pt-24 md:pt-0 md:h-full flex justify-center md:items-center">
                         {children}
                     </div>
                 </section>
-                <section className="col-span-1 w-full h-full flex justify-center items-center">
-                    <Image
-                        src={WelcomeSVG}
-                        alt="Welcome Back"
-                        className="w-[600px]"
-                    />
+                <section className="hidden col-span-1 w-full h-full md:flex justify-center items-center">
+                    <Image src={WelcomeSVG} alt="Welcome Back" className="w-[600px]" />
                 </section>
             </main>
         </>
--- frontend/src/app/(auth)/sign-in/SignInForm.tsx
+++ frontend/src/app/(auth)/sign-in/SignInForm.tsx
@@ -5,13 +5,7 @@ import React from "react";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
-import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormMessage,
-} from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
@@ -36,9 +30,7 @@ const SignInForm = () => {
 
     return (
         <Form {...loginForm}>
-            <form
-                onSubmit={loginForm.handleSubmit(onSubmit)}
-                className="space-y-8">
+            <form onSubmit={loginForm.handleSubmit(onSubmit)} className="space-y-8">
                 <FormField
                     control={loginForm.control}
                     name="email"
@@ -56,10 +48,7 @@ const SignInForm = () => {
                                             {...field}
                                         />
                                         <div className="text-muted-foreground/80 pointer-events-none absolute inset-y-0 end-0 flex items-center justify-center pe-3 peer-disabled:opacity-50">
-                                            <MailIcon
-                                                size={16}
-                                                aria-hidden="true"
-                                            />
+                                            <MailIcon size={16} aria-hidden="true" />
                                         </div>
                                     </div>
                                 </div>
@@ -81,9 +70,7 @@ const SignInForm = () => {
                                             id="password"
                                             className="pe-9"
                                             placeholder="Password"
-                                            type={
-                                                isVisible ? "text" : "password"
-                                            }
+                                            type={isVisible ? "text" : "password"}
                                             {...field}
                                         />
                                         <button
@@ -91,22 +78,15 @@ const SignInForm = () => {
                                             type="button"
                                             onClick={toggleVisibility}
                                             aria-label={
-                                                isVisible
-                                                    ? "Hide password"
-                                                    : "Show password"
+                                                isVisible ? "Hide password" : "Show password"
                                             }
                                             aria-pressed={isVisible}
-                                            aria-controls="password">
+                                            aria-controls="password"
+                                        >
                                             {isVisible ? (
-                                                <EyeOffIcon
-                                                    size={16}
-                                                    aria-hidden="true"
-                                                />
+                                                <EyeOffIcon size={16} aria-hidden="true" />
                                             ) : (
-                                                <EyeIcon
-                                                    size={16}
-                                                    aria-hidden="true"
-                                                />
+                                                <EyeIcon size={16} aria-hidden="true" />
                                             )}
                                         </button>
                                     </div>
@@ -122,7 +102,8 @@ const SignInForm = () => {
                     </Button>
                     <Link
                         href="/sign-up"
-                        className="text-sm text-blue-600 underline underline-offset-2 hover:text-blue-900 hover:scale-110 transition-transform">
+                        className="text-xs md:text-sm text-blue-600 underline underline-offset-2 hover:text-blue-900 hover:scale-110 transition-transform"
+                    >
                         Need an account? Register here
                     </Link>
                 </div>
