b007193fa3ef864b4fefc62ab06649c874a9958d
eedu7 <mueedahmad067@gmail.com>
2025-03-04 17:06:12 +0500

    minor: added the subcategory id, with the foreign key to the category

---
+++ backend/app/models/sub_category.py
@@ -0,0 +1,25 @@
+from uuid import uuid4
+
+from sqlalchemy import UUID, ForeignKey, Unicode
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
+from app.database.mixins import TimeStampMixin, UserAuditMixin
+
+
+class SubCategory(Base, UserAuditMixin, TimeStampMixin):
+    __tablename__ = "sub_categories"
+
+    uuid: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, unique=True, nullable=False, default=uuid4
+    )
+    name: Mapped[str] = mapped_column(Unicode(128), nullable=False, unique=True)
+    category_id: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True), ForeignKey("categories.uuid"), nullable=True
+    )
+
+    def __str__(self):
+        return f"uuid: {self.uuid}, name: {self.name}"
+
+    def __repr__(self):
+        return self.__str__()
