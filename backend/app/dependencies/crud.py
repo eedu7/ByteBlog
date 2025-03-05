4c3bb1a2d356631640d0b90649d2caf99a805419
eedu7 <mueedahmad067@gmail.com>
2025-03-05 07:18:04 +0500

    feat: added the get_post_crud dependency

--- backend/app/dependencies/crud.py
+++ backend/app/dependencies/crud.py
@@ -1,9 +1,14 @@
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.crud import UserCRUD
+from app.crud.post import PostCRUD
+from app.crud.user import UserCRUD
 from app.database import get_async_session
 
 
 def get_user_crud(session: AsyncSession = Depends(get_async_session)) -> UserCRUD:
     return UserCRUD(session=session)
+
+
+def get_post_curd(session: AsyncSession = Depends(get_async_session)) -> PostCRUD:
+    return PostCRUD(session=session)
