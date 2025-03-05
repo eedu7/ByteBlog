417c5b1b46be76faedab42f150662d6a74e0643c
eedu7 <mueedahmad067@gmail.com>
2025-03-05 07:18:41 +0500

    minor: change the get_post_crud function

--- backend/app/dependencies/__init__.py
+++ backend/app/dependencies/__init__.py
@@ -1,4 +1,4 @@
 from .authentication import AuthenticationRequired
 from .crud import get_post_curd, get_user_crud
 
-__all__ = ["AuthenticationRequired", "get_user_crud", "get_post_crud"]
+__all__ = ["AuthenticationRequired", "get_user_crud", "get_post_curd"]
