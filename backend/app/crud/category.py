77d6275065bd1dfb3161d319cd43d2f6bb0bf117
eedu7 <mueedahmad067@gmail.com>
2025-03-06 13:24:55 +0500

    minor: added the docs for the post_category  and category

--- backend/app/crud/category.py
+++ backend/app/crud/category.py
@@ -20,34 +20,68 @@ class CategoryCRUD(BaseCRUD[Category]):
         Args:
             session (AsyncSession): The SQLAlchemy asynchronous session to interact with the database.
         """
-        self.session = session
-        super().__init__(model=Category, session=self.session)
+        super().__init__(model=Category, session=session)
 
     async def get_all_categories(
         self, skip: int = 0, limit: int = 100
     ) -> List[Category]:
-        try:
-            posts = await self.get_by(skip=skip, limit=limit)
-            return posts
-        except Exception as e:
-            raise BadRequestException(f"Exception on fetching all categories. {e}")
+        """
+        Retrieves all categories from the database with pagination support.
 
-    async def get_category_by_uuid(self, uuid: UUID):
+        Args:
+            skip (int, optional): Number of records to skip. Default to 0.
+            limit (int, optional): Maximum number of records to retrieve. Defaults to 100.
+
+        Returns:
+            List[Category]: A list of category objects.
+
+        Raises:
+            BadRequestException: If an error occurs while retrieving categories.
+        """
+        try:
+            return await self.get_by(skip=skip, limit=limit)
+        except Exception as e:
+            raise BadRequestException(f"Failed to fetch categories: {e}")
+
+    async def get_category_by_uuid(self, uuid: UUID) -> Category:
+        """
+        Retrieves a single category by its UUID.
+
+        Args:
+            uuid (UUID): The UUID of the category to retrieve.
+
+        Returns:
+            Category: The category object if found.
+
+        Raises:
+            NotFoundException: If no category is found with the given UUID.
+            BadRequestException: If an error occurs during retrieval.
+        """
         try:
             category = await self.get_by_uuid(uuid)
             if not category:
-                raise NotFoundException(f"No category found by uuid: {str(uuid)}")
+                raise NotFoundException(f"Category with UUID '{uuid}' not found")
             return category
+        except NotFoundException:
+            raise
         except Exception as e:
-            raise BadRequestException(f"Exception on fetching category by uuid. {e}")
+            raise BadRequestException(
+                f"Error retrieving category with UUID {uuid}: {e}"
+            )
 
     async def create_category(self, *, name: str, user_uuid: UUID) -> Category:
         """
-        Create a new category in the database.
+        Creates a new category in the database.
 
         Args:
-            name (str): The name of the category
-            user_uuid (UUID): The UUID of the user, creating the category.
+            name (str): The name of the category.
+            user_uuid (UUID): The UUID of the user creating the category.
+
+        Returns:
+            Category: The newly created category object.
+
+        Raises:
+            BadRequestException: If an error occurs while creating the category.
         """
         data = {"created_by": user_uuid, "name": name}
         try:
@@ -59,6 +93,21 @@ class CategoryCRUD(BaseCRUD[Category]):
     async def update_category(
         self, *, category_uuid: UUID, user_uuid: UUID, updated_name: str
     ) -> bool:
+        """
+        Updates the name of an existing category.
+
+        Args:
+            category_uuid (UUID): The UUID of the category to update.
+            user_uuid (UUID): The UUID of the user making the update.
+            updated_name (str): The new name for the category.
+
+        Returns:
+            bool: True if the update was successful, False otherwise.
+
+        Raises:
+            NotFoundException: If the category is not found.
+            BadRequestException: If an error occurs while updating the category.
+        """
         data = {"updated_by": user_uuid, "name": updated_name}
         category = await self.get_category_by_uuid(category_uuid)
         try:
@@ -70,6 +119,21 @@ class CategoryCRUD(BaseCRUD[Category]):
             raise BadRequestException(f"Exception on updating category. {e}")
 
     async def delete_category(self, category_uuid) -> bool:
+        """
+        Deletes a category from the database.
+
+        Note: This function currently performs a hard delete, but it can be modified for soft deletion in the future.
+
+        Args:
+            category_uuid (UUID): The UUID of the category to delete.
+
+        Returns:
+            bool: True if the deletion was successful, False otherwise.
+
+        Raises:
+            NotFoundException: If the category is not found.
+            BadRequestException: If an error occurs while deleting the category.
+        """
         # TODO: Make it soft delete
         category = await self.get_category_by_uuid(category_uuid)
         try:
--- backend/app/crud/post_category.py
+++ backend/app/crud/post_category.py
@@ -1,68 +1,165 @@
-from typing import Any, Dict
+from typing import Any, Dict, List
 from uuid import UUID
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.base import BaseCRUD
+from app.exceptions import BadRequestException, NotFoundException
 from app.models import PostCategory
 
 
 class PostCategoryCRUD(BaseCRUD[PostCategory]):
     """
-    PostCategory-specific CRUD in the database.
+    CRUD operations for managing post categories in the database.
+    Provides methods for retrieving, creating, updating, and deleting post categories.
     """
 
     def __init__(self, session: AsyncSession):
         """
-        Initializes the PostCategoryCRUD class with the provided async session and PostCategory Model
+        Initializes the PostCategoryCRUD clas with the provided async session.
 
         Args:
-            session (AsyncSession): The SQLAlchemy asynchronous session to interact with the database.
+            session (AsyncSession): The SQLAlachemy asynchronous session to interact with the database.
         """
         super().__init__(model=PostCategory, session=session)
 
-    async def get_all_post_categories(self, skip: int = 0, limit: int = 100):
+    async def get_all_post_categories(
+        self, skip: int = 0, limit: int = 100
+    ) -> List[PostCategory]:
         """
-        Retrieves all post categories from the database.
+        Retrieves all post categories with pagination.
 
         Args:
-            skip (int, optional): The number of post categories to skip. Defaults to 0.
-            limit (int, optional): The maximum number of post categories to retrieve. Defaults to 100.
+            skip (int, optional): The number of records to skip. Defaults to 0.
+            limit (int, optional): The number of records to retrieve. Defaults to 100.
 
         Returns:
-            List[PostCategory]: A list of post categories retrieved from the database.
+            List[PostCategory]: A list of post category objects.
+
+        Raises:
+            BadRequesetException: If there is an error retrieving post categories.
         """
-        return await self.get_by(skip=skip, limit=limit)
+        try:
+            return await self.get_by(skip=skip, limit=limit)
+        except Exception as e:
+            raise BadRequestException(f"Error on retrieving post categories: {e}")
 
     async def get_all_post_categories_by_post_uuid(
         self, post_uuid: str, skip: int = 0, limit: int = 100
-    ):
+    ) -> List[PostCategory]:
         """
-        Retrieves all post categories by post uuid from the database.
+        Retrieves all post categories associated with a specific post UUID.
 
         Args:
-            post_uuid (str): The post uuid to filter post categories.
+            post_uuid (str): The UUID of the post.
+            skip (int, optional): The number of records to skip. Default to 0.
+            limit (int, optional): The number of records to retrieve. Defaults to 100.
 
         Returns:
-            List[PostCategory]: A list of post categories retrieved from the database.
+            List[PostCategory]: A list of post category objects associated with the post.
+
+        Raises:
+            DatabaseException: If there is an error retrieving post categories.
         """
-        filter_ = {"post_uuid": post_uuid}
-        return await self.get_by(filters=filter_, skip=skip, limit=limit)
+        try:
+            filter_ = {"post_uuid": post_uuid}
+            return await self.get_by(filters=filter_, skip=skip, limit=limit)
+        except Exception as e:
+            raise BadRequestException(
+                f"Error retrieving post categories by the post UUID: {e}"
+            )
 
-    async def get_post_category_by_uuid(self, uuid: UUID):
-        return await self.get_by_uuid(uuid=uuid)
+    async def get_post_category_by_uuid(self, uuid: UUID) -> PostCategory:
+        """
+        Retrieves a single post category by its UUID.
 
-    async def create_post_category(self, data: Dict[str, Any], user_uuid: UUID):
-        data.update({"created_by": user_uuid})
-        return await self.create(data)
+        Args:
+            uuid (UUID): The UUID of the post category.
+
+        Returns:
+            PostCategory: The post category object.
+
+        Raises:
+            NotFoundException: If the post category is not found.
+            BadRequestException: If there is an error retrieving the post category.
+        """
+        try:
+            post_category = await self.get_by_uuid(uuid)
+            if not post_category:
+                raise NotFoundException("Post category not found.")
+            return post_category
+        except NotFoundException:
+            raise
+        except Exception as e:
+            raise BadRequestException(f"Error retrieving post category: {e}")
+
+    async def create_post_category(
+        self, data: Dict[str, Any], user_uuid: UUID
+    ) -> PostCategory:
+        """
+        Creates a new post category and associates it with the user who created it.
+
+        Args:
+            data (Dict[str, Any]): The data for the new post category.
+            user_uuid (UUID): The UUID of the user creating the post category.
+
+        Returns:
+            PostCategory: The newly created post category object.
+
+        Raises:
+            BadRequestException: If there is an error creating the post category.
+        """
+        try:
+            data.update({"created_by": user_uuid})
+            return await self.create(data)
+        except Exception as e:
+            raise BadRequestException(f"Error creating post category: {e}")
 
     async def update_post_category(
         self, data: Dict[str, any], post_category_uuid: UUID, user_uuid: UUID
-    ):
-        data.update({"updated_by": user_uuid})
-        post_category = await self.get_post_category_by_uuid(post_category_uuid)
-        await self.update(post_category, data)
+    ) -> bool:
+        """
+        Updates an existing post category.
 
-    async def delete_post_category(self, post_category_uuid: UUID):
-        post_category = await self.get_post_category_by_uuid(post_category_uuid)
-        await self.delete(post_category)
+        Args:
+            data (Dict[str, Any]): The updated data for the post category.
+            post_category_uuid (UUID): The UUID of the post category to be updated.
+            user_uuid (UUID): The UUID of the user making the update.
+
+        Returns:
+            bool: True if the update was successful, False otherwise.
+
+        Raises:
+            NotFoundException: If the post category is not found.
+            BadRequestException: If there is an error updating the post category.
+        """
+        try:
+            data.update({"updated_by": user_uuid})
+            post_category = await self.get_post_category_by_uuid(post_category_uuid)
+            await self.update(post_category, data)
+        except NotFoundException:
+            raise
+        except Exception as e:
+            BadRequestException(f"Error updating post category: {e}")
+
+    async def delete_post_category(self, post_category_uuid: UUID) -> bool:
+        """
+        Deletes a post category from the database.
+
+        Args:
+            post_category_uuid (UUID): The UUID of the post category to be deleted.
+
+        Returns:
+            bool: True if deletion was successful, False otherwise.
+
+        Raises:
+            NotFoundException: If the post category is not found.
+            BadRequestException: If there is an error deleting the post category.
+        """
+        try:
+            post_category = await self.get_post_category_by_uuid(post_category_uuid)
+            await self.delete(post_category)
+        except NotFoundException:
+            raise
+        except Exception as e:
+            raise BadRequestException(f"Error deleting post category: {e}")
