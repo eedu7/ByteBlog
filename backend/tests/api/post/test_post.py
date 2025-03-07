ed2e562b7cc743480f746c9c2cac78fe55091023
eedu7 <mueedahmad067@gmail.com>
2025-03-07 13:21:24 +0500

    test: add partial update tests for post API endpoint

--- backend/tests/api/post/test_post.py
+++ backend/tests/api/post/test_post.py
@@ -4,6 +4,7 @@ import pytest
 from httpx import AsyncClient
 from icecream import ic
 
+from app.schemas.post import PostStatus
 from tests.utils.users import create_fake_user
 
 API_ENDPOINT = "/post"
@@ -335,8 +336,165 @@ async def test_update_post_without_auth(
     assert response.json()["message"]
 
 
-# @pytest.mark.asyncio
-# async def partial_update_post(): ...
+@pytest.mark.asyncio
+async def test_partial_update_post(
+    client: AsyncClient,
+    mock_post_data: Dict[str, Any],
+    mock_post_update_data: Dict[str, Any],
+):
+    user_data = create_fake_user()
+
+    response = await client.post("/auth/register", json=user_data)
+
+    access_token = response.json()["token"]["access_token"]
+    created_post = await client.post(
+        f"{API_ENDPOINT}/",
+        json=mock_post_data,
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
+    post_uuid = created_post.json()["post"]["uuid"]
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    response = await client.patch(
+        f"{API_ENDPOINT}/{post_uuid}", json=mock_post_update_data, headers=headers
+    )
+
+    updated_post = await client.get(f"{API_ENDPOINT}/{post_uuid}")
+
+    updated_post_data = updated_post.json()
+
+    assert response.status_code == 204
+    assert updated_post_data["title"] == mock_post_update_data["title"]
+    assert updated_post_data["body"] == mock_post_update_data["body"]
+    assert updated_post_data["status"] == mock_post_update_data["status"]
+
+
+@pytest.mark.asyncio
+async def test_partial_update_post_title(
+    client: AsyncClient,
+    mock_post_data: Dict[str, Any],
+    mock_post_update_data: Dict[str, Any],
+):
+    user_data = create_fake_user()
+
+    response = await client.post("/auth/register", json=user_data)
+
+    access_token = response.json()["token"]["access_token"]
+    created_post = await client.post(
+        f"{API_ENDPOINT}/",
+        json=mock_post_data,
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
+    post_uuid = created_post.json()["post"]["uuid"]
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    data = {"title": "Updated title"}
+    response = await client.patch(
+        f"{API_ENDPOINT}/{post_uuid}", json=data, headers=headers
+    )
+
+    updated_post = await client.get(f"{API_ENDPOINT}/{post_uuid}")
+
+    updated_post_data = updated_post.json()
+
+    assert response.status_code == 204
+    assert updated_post_data["title"] == data["title"]
+
+
+@pytest.mark.asyncio
+async def test_partial_update_post_body(
+    client: AsyncClient,
+    mock_post_data: Dict[str, Any],
+):
+    user_data = create_fake_user()
+
+    response = await client.post("/auth/register", json=user_data)
+
+    access_token = response.json()["token"]["access_token"]
+    created_post = await client.post(
+        f"{API_ENDPOINT}/",
+        json=mock_post_data,
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
+    post_uuid = created_post.json()["post"]["uuid"]
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    data = {"body": "Updated bodyd"}
+    response = await client.patch(
+        f"{API_ENDPOINT}/{post_uuid}", json=data, headers=headers
+    )
+
+    updated_post = await client.get(f"{API_ENDPOINT}/{post_uuid}")
+
+    updated_post_data = updated_post.json()
+
+    assert response.status_code == 204
+    assert updated_post_data["body"] == data["body"]
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "data",
+    [
+        ({"status": PostStatus.ARCHIVED}),
+        ({"status": PostStatus.DELETED}),
+        ({"status": PostStatus.DRAFT}),
+        ({"status": PostStatus.PUBLISHED}),
+    ],
+)
+async def test_partial_update_post_status(
+    client: AsyncClient, mock_post_data: Dict[str, Any], data: Dict[str, Any]
+):
+    user_data = create_fake_user()
+
+    response = await client.post("/auth/register", json=user_data)
+
+    access_token = response.json()["token"]["access_token"]
+    created_post = await client.post(
+        f"{API_ENDPOINT}/",
+        json=mock_post_data,
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
+    post_uuid = created_post.json()["post"]["uuid"]
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    response = await client.patch(
+        f"{API_ENDPOINT}/{post_uuid}", json=data, headers=headers
+    )
+
+    updated_post = await client.get(f"{API_ENDPOINT}/{post_uuid}")
+
+    updated_post_data = updated_post.json()
+
+    assert response.status_code == 204
+    assert updated_post_data["status"] == data["status"]
+
+
+@pytest.mark.asyncio
+async def test_partial_update_post_without_auth(
+    client: AsyncClient,
+    mock_post_data: Dict[str, Any],
+    mock_post_update_data: Dict[str, Any],
+):
+    user_data = create_fake_user()
+
+    response = await client.post("/auth/register", json=user_data)
+
+    access_token = response.json()["token"]["access_token"]
+    created_post = await client.post(
+        f"{API_ENDPOINT}/",
+        json=mock_post_data,
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
+    post_uuid = created_post.json()["post"]["uuid"]
+
+    response = await client.patch(
+        f"{API_ENDPOINT}/{post_uuid}", json=mock_post_update_data
+    )
+    ic(response.json())
+
+    assert response.status_code == 401
+    assert response.json()["message"]
 
 
 # @pytest.mark.asyncio
